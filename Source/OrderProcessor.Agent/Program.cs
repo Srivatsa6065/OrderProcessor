84a4ca5df1ddd237d091834eb87778c53fe22dc1
Srivatsa Prasad <Srivatsa.Prasad@readynetworks.com>
2021-06-03 15:58:30 +0530

    project init

---
+++ Source/OrderProcessor.Agent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrderProcessor.Agent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
